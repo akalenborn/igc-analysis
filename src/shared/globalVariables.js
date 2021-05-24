6f97095afd72ddc84fb4c7ebd06fce4605f66d65
ademes <ademes.sebastian@gmail.com>
2021-05-24 07:49:33 +0200

    Update globalVariables.js

--- src/shared/globalVariables.js
+++ src/shared/globalVariables.js
@@ -1,13 +1,14 @@
 const checkboxContainer = document.querySelector('#checkbox-container');
 const preferenceContainer = document.querySelector('#preferences');
+const igcInfoContainer = document.querySelector('#igc-info-container');
 const curve90Checkbox = document.querySelector('#curve-90');
 const curve180Checkbox = document.querySelector('#curve-180');
 const circleCheckbox = document.querySelector('#circle-checkbox');
 const eightCheckbox = document.querySelector('#eight-checkbox');
 const triangleCheckbox = document.querySelector('#triangle-checkbox');
-const igcInfoContainer = document.querySelector('#igc-info-container');
+const triangleResultContainer = document.querySelector('#triangle-result-container');
 const triangleInfoContainer = document.querySelector('#triangle-info-container');
-const triangleRuntimeContainer = document.querySelector('#triangle-runtime');
+const triangleRuntimeContainer = document.querySelector('#triangle-runtime-container');
 //const outputContainer = document.querySelector('#analysis-output');
 //const circleDetectionContainer = document.querySelector('.circle-detection');
 //const dragAndDropParagraph = document.querySelector('#drag-and-drop-paragraph');
@@ -24,11 +25,12 @@ const altitudeUnits = document.querySelector('#select-altitude-units');
 const timeZoneSelect = document.querySelector('#select-time-zone');
 const curveAlgorithm = document.querySelector('#select-curve-algorithm');
 const circleAlgorithm = document.querySelector('#select-circle-algorithm');
+const triangleAlgorithm = document.querySelector('#select-triangle-algorithm');
 const errorMessageElement = document.querySelector('#errorMessage');
 const timeBackButton = document.querySelector('#timeBack');
 const timeForwardButton = document.querySelector('#timeForward');
 const fileControl = document.querySelector('#file-control');
-const triangleAlgorithm = document.querySelector('#select-triangle-algorithm');
+const pendingBox = document.querySelector('#algorithm-processing-container');
 
 let igcFile;
 let mapControl;
@@ -52,9 +54,9 @@ let results = {
 };
 
 let algorithms = [
-    {name: "curve90", result: results.shapeDetection.curve90, checkbox: curve90Checkbox, color: "#32cd32"},
-    {name: "curve180", result: results.shapeDetection.curve180, checkbox: curve180Checkbox, color: "#00FF00"},
-    {name: "circle", result: results.shapeDetection.circle, checkbox: circleCheckbox, color: "blue"},
-    {name: "eight", result: results.shapeDetection.eight, checkbox: eightCheckbox, color: "yellow"},
-    {name: "triangle", result: results.shapeDetection.triangle, checkbox: triangleCheckbox, color: "green"}
+    {name: "curve90", result: results.shapeDetection.curve90, checkbox: curve90Checkbox, default: defaultCurve, alg: curveAlgorithm, color: "#32cd32"},
+    {name: "curve180", result: results.shapeDetection.curve180, checkbox: curve180Checkbox, default: defaultCurve, alg: curveAlgorithm, color: "#00FF00"},
+    {name: "circle", result: results.shapeDetection.circle, checkbox: circleCheckbox, default: defaultCircle, alg: circleAlgorithm, color: "blue"},
+    {name: "eight", result: results.shapeDetection.eight, checkbox: eightCheckbox, default: defaultEight, color: "yellow"},
+    {name: "triangle", result: results.shapeDetection.triangle, checkbox: triangleCheckbox, default: defaultTriangle, alg: triangleAlgorithm, color: "green"}
 ];
